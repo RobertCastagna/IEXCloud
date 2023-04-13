028904b4b1715f9c3c371b291cdc3ea7f8f81269
Robert Castagna <49927397+RobertCastagna@users.noreply.github.com>
2023-04-13 18:00:52 -0400

    updates

--- trading_analytics/trading_analytics/assets.py
+++ trading_analytics/trading_analytics/assets.py
@@ -1,1 +1,16 @@
 
+import requests as re
+import os
+import json
+import pandas as pd
+
+#base_url = 'https://api.iex.cloud/v1/'
+#api_token = '?token=' + os.environ["API_KEY"]
+#quote = re.get(base_url + '/data/core/quote/aapl' + api_token)
+
+os.chdir(r"D:\Data")
+df = pd.read_csv("original.csv", header=0)
+
+
+print(df.head())
+
